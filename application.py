1d97ac101e7d4e0b0c4691d96b71a9ec0d0c8586
shivam gupta <shivam.gupta.bpl003@gmail.com>
2024-03-06 03:25:40 +0530

    review 2 final

--- application.py
+++ application.py
@@ -1,83 +1,189 @@
-# import pickle
-# from flask import Flask,request,jsonify,render_template, send_file
+# # import pickle
+# # from flask import Flask,request,jsonify,render_template, send_file
+# # import numpy as np
+# # import pandas as pd
+# # from sklearn.preprocessing import StandardScaler
+# # import matplotlib.pyplot as plt
+
+# # application = Flask(__name__)
+# # app=application
+
+# # model_fit = pickle.load(open("/Users/damodargupta/Desktop/EPICS-PROJECT/sarimax_model.pkl", "rb"))
+
+# # ## Route for home page
+# # @app.route('/')
+# # def index():
+# #     return render_template('index.html')
+
+# # def get_inflation_rate_range(df, start_year, end_year):
+# #     df = pd.read_excel("/Users/damodargupta/Desktop/EPICS-PROJECT/inflation_ratings.xlsx")
+# #     # Filter the dataframe for the given range of years
+# #     df_range = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
+# #     # Melt the dataframe to have 'months' and 'inflation ratings' in separate columns
+# #     df_melted = df_range.melt(id_vars=['Year'], var_name='Month', value_name='Inflation Rate')
+# #     return df_melted
+
+# # @app.route('/predictdata',methods=['GET','POST'])
+# # def predict_datapoint():
+# #     if request.method=='POST':
+# #         start = int(request.form.get('start'))
+# #         end = int(request.form.get('end'))
+# #         event = float(request.form.get('event'))
+# #         if event == "Ukraine-Russia War":
+# #             event_list = [(pd.to_datetime('2022-02-24'), 'Start of\nRussian Invasion', 'purple')]
+# #         else:
+# #             event_list = [ (pd.to_datetime('2023-03-31'), 'Start of\nForecast', 'crimson')]
+            
+# #         yhat = model_fit.predict(start=start, end=end, typ='levels', dynamic=False).rename('Predict')
+        
+# #         # forecast and confidence intervals 70% and 95%
+# #         conf_int_70 = model_fit.get_forecast(steps=6).summary_frame(alpha=0.3)
+# #         conf_int_95 = model_fit.get_forecast(steps=6).summary_frame(alpha=0.05)
+        
+# #         # extract index list
+# #         def index_list():
+# #             conf_interval = conf_int_95.reset_index()
+# #             return conf_interval['index'].to_list()
+
+# #         indx_list = index_list()
+
+# #         # plot events
+# #         for date_point, label, clr in event_list:
+# #             plt.axvline(x=date_point, color=clr, linestyle=':')
+# #             plt.text(x=date_point, y=23, s=label, horizontalalignment='center', verticalalignment='center',
+# #                     color=clr, bbox=dict(facecolor='white', alpha=0.9, boxstyle='round, pad=1', linewidth=0.2))
+            
+# #         df = pd.read_excel("/Users/damodargupta/Desktop/EPICS-PROJECT/inflation_ratings.xlsx")
+# #         inflation_rate_range_df = get_inflation_rate_range(df, start, end)
+# #         series = inflation_rate_range_df["Inflation Rate"].head()
+        
+# #         # 2nd plot - forecasting results
+# #         series.plot(legend=True, label='Actual', figsize=(16, 6), color='orange')
+# #         yhat.plot(legend=True, color='green')
+# #         conf_int_95['mean'].plot(legend=True, label='Forecast', color='crimson')
+# #         # confidence intervals 95%
+# #         plt.fill_between(x=indx_list, y1=conf_int_95['mean_ci_upper'], y2=conf_int_95['mean_ci_lower'],
+# #                         alpha=0.4, label='95% Confidence\nInterval', linewidth=0)
+# #         # confidence intervals 70%
+# #         plt.fill_between(x=indx_list, y1=conf_int_70['mean_ci_upper'], y2=conf_int_70['mean_ci_lower'],
+# #                         alpha=0.6, label='70% Confidence\nInterval', linewidth=0)
+# #         plt.xlim(['2020-05', '2023-09'])
+# #         plt.title('Inflation Forecast: Mar 2023 - Aug 2023')
+# #         plt.legend(loc='upper left')
+# #         plt.show()
+
+# # if __name__=="__main__":
+# #     app.run(host="0.0.0.0" , port=8000)
+
+# import io
+# import base64
+# from flask import Flask, request, render_template
 # import numpy as np
 # import pandas as pd
-# from sklearn.preprocessing import StandardScaler
 # import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('Agg')
+# import pickle
+# from datetime import datetime
 
 # application = Flask(__name__)
-# app=application
+# app = application
 
+# # Replace the path with your actual path
 # model_fit = pickle.load(open("/Users/damodargupta/Desktop/EPICS-PROJECT/sarimax_model.pkl", "rb"))
 
-# ## Route for home page
+# # ... (your existing functions)
+
 # @app.route('/')
 # def index():
-#     return render_template('index.html')
+#     return render_template('index-2.html')
 
-# def get_inflation_rate_range(df, start_year, end_year):
-#     df = pd.read_excel("/Users/damodargupta/Desktop/EPICS-PROJECT/inflation_ratings.xlsx")
-#     # Filter the dataframe for the given range of years
-#     df_range = df[(df['Year'] >= start_year) & (df['Year'] <= end_year)]
-#     # Melt the dataframe to have 'months' and 'inflation ratings' in separate columns
-#     df_melted = df_range.melt(id_vars=['Year'], var_name='Month', value_name='Inflation Rate')
-#     return df_melted
+# def get_inflation_rate_range(df, start, end):
+#     dataframe = pd.read_csv("/Users/damodargupta/Desktop/EPICS-PROJECT/date-wise-data.csv")
+#     df['Date'] = pd.to_datetime(df['Date'])
+#     # Create filtered DataFrame
+#     filtered_df = df[(df['Date'] >= start) & (df['Date'] <= end)]
+#     plt.plot(filtered_df['Date'], filtered_df['Rate'], marker='o', linestyle='-')
 
-# @app.route('/predictdata',methods=['GET','POST'])
+# @app.route('/predictdata', methods=['GET', 'POST'])
 # def predict_datapoint():
-#     if request.method=='POST':
-#         start = int(request.form.get('start'))
-#         end = int(request.form.get('end'))
-#         event = float(request.form.get('event'))
+#     if request.method == 'POST':
+#         # Parse date strings to datetime objects
+#         start_date = request.form.get('start_date')
+#         print(start_date)
+#         end_date = request.form.get('end_date')
+#         print(end_date)
+#         event = request.form.get('event')
+#         print(event)
+
 #         if event == "Ukraine-Russia War":
-#             event_list = [(pd.to_datetime('2022-02-24'), 'Start of\nRussian Invasion', 'purple')]
+#             event_list = [(pd.to_datetime(start_date), 'Start of\nRussian Invasion', 'purple'), (pd.to_datetime("2022-02-28"), 'Start of\nForecast', 'crimson')]
+#             yhat = model_fit.predict(start=pd.to_datetime("2022-02-28"), end=end_date, typ='levels', dynamic=False).rename('Predict')
+#             print(event_list)
+#         elif event == "Covid 19 Pandemic":
+#             event_list = [(pd.to_datetime(start_date), 'Start of\nForecast', 'purple'), (pd.to_datetime("2020-01-31"), 'Start of\nForecast', 'crimson')]
+#             yhat = model_fit.predict(start=pd.to_datetime("2020-01-31"), end=end_date, typ='levels', dynamic=False).rename('Predict')
+#             print(event_list)
 #         else:
-#             event_list = [ (pd.to_datetime('2023-03-31'), 'Start of\nForecast', 'crimson')]
+#             event_list = [(pd.to_datetime(start_date), 'Start of\nRussian Invasion', 'purple'), (pd.to_datetime(end_date), 'Start of\nForecast', 'crimson')]
+#             yhat = model_fit.predict(start=pd.to_datetime("2020-01-31"), end=end_date, typ='levels', dynamic=False).rename('Predict')
             
-#         yhat = model_fit.predict(start=start, end=end, typ='levels', dynamic=False).rename('Predict')
-        
-#         # forecast and confidence intervals 70% and 95%
 #         conf_int_70 = model_fit.get_forecast(steps=6).summary_frame(alpha=0.3)
 #         conf_int_95 = model_fit.get_forecast(steps=6).summary_frame(alpha=0.05)
-        
-#         # extract index list
+
 #         def index_list():
 #             conf_interval = conf_int_95.reset_index()
 #             return conf_interval['index'].to_list()
 
 #         indx_list = index_list()
+#         plt.figure(figsize=(16, 6))
+#         if event== "None":
+#             plt.axvline(x=pd.to_datetime("2020-01-31"), color="purple", linestyle=':')
+#             plt.text(x=pd.to_datetime("2020-01-31"), y=17, s="Start of Forecast", horizontalalignment='center', verticalalignment='center',
+#                     color="crimson", bbox=dict(facecolor='white', alpha=0.9, boxstyle='round, pad=1', linewidth=0.2))
+#         else:
+#             plt.axvline(x=event_list[1][0], color="purple", linestyle=':')
+#             plt.text(x=event_list[1][0], y=17, s="Start of Forecast", horizontalalignment='center', verticalalignment='center',
+#                     color="crimson", bbox=dict(facecolor='white', alpha=0.9, boxstyle='round, pad=1', linewidth=0.2))
 
-#         # plot events
-#         for date_point, label, clr in event_list:
-#             plt.axvline(x=date_point, color=clr, linestyle=':')
-#             plt.text(x=date_point, y=23, s=label, horizontalalignment='center', verticalalignment='center',
-#                     color=clr, bbox=dict(facecolor='white', alpha=0.9, boxstyle='round, pad=1', linewidth=0.2))
-            
-#         df = pd.read_excel("/Users/damodargupta/Desktop/EPICS-PROJECT/inflation_ratings.xlsx")
-#         inflation_rate_range_df = get_inflation_rate_range(df, start, end)
-#         series = inflation_rate_range_df["Inflation Rate"].head()
-        
-#         # 2nd plot - forecasting results
-#         series.plot(legend=True, label='Actual', figsize=(16, 6), color='orange')
+#         df = pd.read_csv("/Users/damodargupta/Desktop/EPICS-PROJECT/date-wise-data.csv")
+#         get_inflation_rate_range(df, start_date, end_date)    
 #         yhat.plot(legend=True, color='green')
 #         conf_int_95['mean'].plot(legend=True, label='Forecast', color='crimson')
-#         # confidence intervals 95%
 #         plt.fill_between(x=indx_list, y1=conf_int_95['mean_ci_upper'], y2=conf_int_95['mean_ci_lower'],
-#                         alpha=0.4, label='95% Confidence\nInterval', linewidth=0)
-#         # confidence intervals 70%
+#                          alpha=0.4, label='95% Confidence\nInterval', linewidth=0)
 #         plt.fill_between(x=indx_list, y1=conf_int_70['mean_ci_upper'], y2=conf_int_70['mean_ci_lower'],
-#                         alpha=0.6, label='70% Confidence\nInterval', linewidth=0)
-#         plt.xlim(['2020-05', '2023-09'])
-#         plt.title('Inflation Forecast: Mar 2023 - Aug 2023')
+#                          alpha=0.6, label='70% Confidence\nInterval', linewidth=0)
+#         plt.xlim([start_date, end_date])
+#         if event == "Ukraine-Russia War":
+#             plt.title(f'Inflation Forecast: 2022-02-28 to {end_date}')
+#         elif event == "Covid 19 Pandemic":
+#             plt.title(f'Inflation Forecast: 2020-01-31 to {end_date}')
+#         else: 
+#             plt.title(f'Inflation Forecast: 2020-01-31 to {end_date}')
 #         plt.legend(loc='upper left')
-#         plt.show()
+#         plt.savefig('/Users/damodargupta/Desktop/EPICS-PROJECT/plot.png', format='png')
 
-# if __name__=="__main__":
-#     app.run(host="0.0.0.0" , port=8000)
+#         # Save the plot to BytesIO object
+#         plot_img = io.BytesIO()
+#         plt.savefig(plot_img, format='png')
+#         plot_img.seek(0)
+
+#         # Encode the plot image as base64
+#         plot_img_base64 = base64.b64encode(plot_img.getvalue()).decode('utf-8')
+
+#         # Render the template with the plot image
+#         return render_template('index-2.html', plot_img_base64=plot_img_base64)
+
+#     else:
+#         return render_template('index-2.html')
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=8000)
 
 import io
 import base64
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -85,21 +191,22 @@ import matplotlib
 matplotlib.use('Agg')
 import pickle
 from datetime import datetime
+from statsmodels.tsa.statespace.sarimax import SARIMAXResults
 
 application = Flask(__name__)
 app = application
 
 # Replace the path with your actual path
-model_fit = pickle.load(open("/Users/damodargupta/Desktop/EPICS-PROJECT/sarimax_model.pkl", "rb"))
+model_fit = pickle.load(open("sarimax_model.pkl", "rb"))
 
 # ... (your existing functions)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index-2.html')
 
 def get_inflation_rate_range(df, start, end):
-    dataframe = pd.read_csv("/Users/damodargupta/Desktop/EPICS-PROJECT/date-wise-data.csv")
+    dataframe = pd.read_csv("date-wise-data.csv")
     df['Date'] = pd.to_datetime(df['Date'])
     # Create filtered DataFrame
     filtered_df = df[(df['Date'] >= start) & (df['Date'] <= end)]
@@ -109,11 +216,12 @@ def get_inflation_rate_range(df, start, end):
 def predict_datapoint():
     if request.method == 'POST':
         # Parse date strings to datetime objects
-        start_date = request.form.get('start_date')
+        data = request.json
+        start_date = data.get('start-date')
         print(start_date)
-        end_date = request.form.get('end_date')
+        end_date = data.get('end-date')
         print(end_date)
-        event = request.form.get('event')
+        event = data.get('event')
         print(event)
 
         if event == "Ukraine-Russia War":
@@ -136,7 +244,7 @@ def predict_datapoint():
             return conf_interval['index'].to_list()
 
         indx_list = index_list()
-        plt.figure(figsize=(16, 6))
+        plt.figure(figsize=(10.5,7))
         if event== "None":
             plt.axvline(x=pd.to_datetime("2020-01-31"), color="purple", linestyle=':')
             plt.text(x=pd.to_datetime("2020-01-31"), y=17, s="Start of Forecast", horizontalalignment='center', verticalalignment='center',
@@ -146,7 +254,7 @@ def predict_datapoint():
             plt.text(x=event_list[1][0], y=17, s="Start of Forecast", horizontalalignment='center', verticalalignment='center',
                     color="crimson", bbox=dict(facecolor='white', alpha=0.9, boxstyle='round, pad=1', linewidth=0.2))
 
-        df = pd.read_csv("/Users/damodargupta/Desktop/EPICS-PROJECT/date-wise-data.csv")
+        df = pd.read_csv("date-wise-data.csv")
         get_inflation_rate_range(df, start_date, end_date)    
         yhat.plot(legend=True, color='green')
         conf_int_95['mean'].plot(legend=True, label='Forecast', color='crimson')
@@ -162,7 +270,7 @@ def predict_datapoint():
         else: 
             plt.title(f'Inflation Forecast: 2020-01-31 to {end_date}')
         plt.legend(loc='upper left')
-        plt.savefig('/Users/damodargupta/Desktop/EPICS-PROJECT/plot.png', format='png')
+        plt.savefig('plot.png', format='png')
 
         # Save the plot to BytesIO object
         plot_img = io.BytesIO()
@@ -170,13 +278,96 @@ def predict_datapoint():
         plot_img.seek(0)
 
         # Encode the plot image as base64
-        plot_img_base64 = base64.b64encode(plot_img.getvalue()).decode('utf-8')
+        plot_img_base64 = base64.b64encode(plot_img.getvalue()).decode()
+        plt.close()
+        
 
         # Render the template with the plot image
-        return render_template('index.html', plot_img_base64=plot_img_base64)
+        # return render_template('index-2.html', plot_img_base64=plot_img_base64)
+        return jsonify({'plot_img_base64': plot_img_base64})
 
     else:
-        return render_template('index.html')
+        return render_template('index-2.html')
+    
+@app.route('/about')
+def about():
+    return render_template('team_members.html')
+
+def load_model(category):
+    # Load the SARIMAX model from the file
+    model_path = f"models/{category.replace(' ', '_')}_model.pkl"
+    return SARIMAXResults.load(model_path)
+
+# Function to generate a forecast using the loaded model
+def generate_forecast(category, start_date, end_date):
+    # Load the model for the specified category
+    model = load_model(category)
+    
+    # Assuming you have a DataFrame or other data source for generating the forecast
+    # Here we'll create a DataFrame with a date range between start_date and end_date
+    date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+    forecast_data = pd.DataFrame(index=date_range)
+    
+    # Generate the forecast
+    forecast_result = model.predict(start=start_date, end=end_date)
+    
+    # Add the forecast values to the result dictionary
+    forecast_result['forecast_values'] = forecast_result.tolist()
+    
+    return forecast_result
+
+def add_months_to_date_range(start_date, end_date):
+    # Generate a date range with monthly frequency
+    date_range = pd.date_range(start=start_date, end=end_date, freq='MS').strftime('%Y-%m-%d').tolist()
+    return date_range
+
+@app.route('/predictdatacategorical', methods=['GET', 'POST'])
+def predictdatacategorical():
+    data = request.json  # Get the JSON data sent from the client
+    category = data.get('category')
+    start_date = data.get('start_date')
+    end_date = data.get('end_date')
+
+    if not all([category, start_date, end_date]):
+        # If any of the fields are missing, you can return an error message
+        return jsonify({'error': 'Missing data!'}), 400
+
+    # Continue processing the data and generating forecast
+    forecast_result = generate_forecast(category, start_date, end_date)
+    print(forecast_result)
+
+    # Since you are expecting to render a template, make sure you pass
+    # all the required context variables to the template rendering function.
+    # Generate the plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(add_months_to_date_range(start_date=start_date, end_date=end_date), forecast_result['forecast_values'], marker='o', linestyle='-')
+    plt.title(f'Forecast from {start_date} to {end_date}')
+    plt.xlabel('Date')
+    plt.xticks(rotation = 90 ,  fontsize = 5)
+
+    plt.ylabel('Forecast Values')
+    
+    # Save the plot to a BytesIO object
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    plt.close()
+    bytes_image.seek(0)
+
+    # Encode the image to base64 string and decode to UTF-8
+    base64_png = base64.b64encode(bytes_image.getvalue()).decode('utf-8')
+    
+    return jsonify({
+        'success': True,
+        'data': {
+            'forecast_result': forecast_result.tolist(),  # Convert to list
+            'image_base64':  base64_png,
+            'category': category,
+            'start_date': start_date,
+            'end_date': end_date
+        }
+    })
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True , port=1111)
+
---
+++ static/js/script.js
@@ -0,0 +1,262 @@
+function submitForm(){
+    var start_date = document.getElementById("start-date").value;
+    var end_date = document.getElementById("end-date").value;
+    var checkbox_covid = document.getElementById('covid');
+    var checkbox_war = document.getElementById('war');
+    var event;
+
+    if (checkbox_covid.checked){
+        if(checkbox_war.checked){
+            event = 'Both';
+        }
+        else{
+            event = "Covid 19 Pandemic"
+        }
+    }
+    else if (checkbox_war.checked){
+        event="Ukraine-Russia War";
+    }
+    else{
+        event="None"
+    }
+    
+    console.log(event)
+
+    var formData = {
+        'start-date': start_date,
+        'end-date': end_date,
+        'event': event
+    };
+
+    sendToFlask(formData)
+
+}
+
+
+function sendToFlask(data){ 
+    fetch('/predictdata', {
+        method: 'POST',
+        headers:{
+            'content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    .then (response => {
+        if (response.ok){
+            console.log('vinit')
+            return response.json();
+            
+        }
+        throw new Error('Network response was not ok.')
+    })
+    .then(responseData => {
+        console.log('Response from Flask:', responseData)
+
+        var plot_img_base64 = responseData.plot_img_base64;
+        var img = new Image();
+
+        img.src = 'data:image/png;base64,'+ plot_img_base64;
+        
+        var chartDiv = document.getElementById('graph');
+        chartDiv.innerHTML = '';
+        chartDiv.append(img);
+    })
+    .catch(error => {
+        console.error('Error:', error)
+    })
+    // var plot_img_base64 = "data:image/png;base64, {{ plot_img_base64 }}";
+
+    // var canvas = document.getElementById('chart-line');
+    // var ctx = canvas.getContext('2d');
+
+    // var img =new Image();
+    // img.onload = function(){
+    //     ctx.drawImage(img,0,0, canvas.height);
+    // };
+    // img.src = plot_img_base64;
+    
+    
+}
+
+// function sendToFlask2(data){ 
+//     fetch('/predictdatacategorical', {
+//         method: 'POST',
+//         headers:{
+//             'content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     })
+//     .then (response => {
+//         if (response.ok){
+//             console.log('vinit')
+//             return response.json();
+            
+//         }
+//         throw new Error('Network response was not ok.')
+//     })
+//     .then(responseData => {
+//         console.log('Response from Flask:', responseData)
+
+//         var plot_img_base64 = responseData.plot_img_base64;
+//         var img = new Image();
+
+//         img.src = 'data:image/png;base64,'+ plot_img_base64;
+        
+//         var chartDiv = document.getElementById('graph');
+//         chartDiv.innerHTML = '';
+//         chartDiv.append(img);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error)
+//     })
+    // var plot_img_base64 = "data:image/png;base64, {{ plot_img_base64 }}";
+
+    // var canvas = document.getElementById('chart-line');
+    // var ctx = canvas.getContext('2d');
+
+    // var img =new Image();
+    // img.onload = function(){
+    //     ctx.drawImage(img,0,0, canvas.height);
+    // };
+    // img.src = plot_img_base64;
+    
+    
+// }
+
+
+
+// const availableWords = [
+//   "Category 1", "Category 2", "Category 3", "Category 4", "Category 5", "Category 6", "Category 7", "Category 8", "Category 9", "Category 10", "Eggs", "Milk", "Bread"
+// ];
+
+// const resultBox = document.getElementById(".result-box"); 
+// const inputBox = document.getElementById("input-box"); 
+
+// inputBox.onkeyup = function(){
+//   let result = [];
+//   let input = inputBox.value;
+//   if(input.length){
+//       result = availableWords.filter((keyword)=>{
+//          return keyword.toLowerCase().includes(input.toLowerCase());
+//       });
+//       console.log(result);
+//   }
+//   display(result);
+
+//   if(!result.length){
+//       resultBox.innerHTML='';
+//   }
+// }
+
+// function display(result){
+//   const content = result.map((list)=>{
+//       return "<li onclick=selectInput(this)>"+ list + "</li>";
+//   });
+
+//   resultBox.innerHTML = "<ul>"+ content.join('') + "</ul>";
+// }
+
+function toggleSearch() {
+  var container = document.getElementById("searchContainer");
+  container.style.display = (container.style.display === "none") ? "block" : "none";
+  if (container.style.display === "block") {
+      fillCategories();
+  }
+}
+
+var selectedCategory = null;
+
+function fillCategories() {
+    var categories = ["Cereals and products", "Clothing", "Clothing and footwear", "Consumer Food Price Index", "Education", "Egg", "Food and beverages", "Footwear", "Fruits", "Fuel and light", "Health", "Household goods and services", "Housing", "Meat and fish", "Milk and products", "Miscellaneous", "Non-alcoholic beverages", "Oils and fats", "Pan; tobacco; and intoxicants", "Personal care and effects", "Prepared meals; snacks; sweets etc.", "Pulses and products", "Recreation and amusement", "Spices", "Sugar and confectionery", "Transport and communication", "Vegetables"];
+    var categoriesList = document.getElementById("categoriesList");
+    categoriesList.innerHTML = "";
+  
+    categories.forEach(function(category) {
+        var categoryButton = document.createElement("button");
+        categoryButton.textContent = category;
+        categoryButton.className = "category-button"; // Add a class for styling if needed
+        categoryButton.onclick = function() {
+            selectedCategory = category; // Set the global variable on click
+        };
+        categoriesList.appendChild(categoryButton);
+    });
+}
+  
+  function sendCategoryData() {
+    if (selectedCategory === null) {
+        console.error('No category selected.');
+        return;
+    }
+    var start_date = document.getElementById("start-date").value;
+    var end_date = document.getElementById("end-date").value;
+    
+    // Use the globally selected category
+    var data = {
+      'category': selectedCategory,
+      'start_date': start_date,
+      'end_date': end_date
+    };
+    
+    console.log(data)
+    // Send the data to the Flask server
+    fetch('/predictdatacategorical', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('response' + response.ok)
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+        console.log('Success:', data);
+        var imageSrc = 'data:image/png;base64,' + data.data.image_base64;
+        console.log(imageSrc)
+        var imgElement = document.createElement('img');
+        imgElement.src = imageSrc;
+        document.getElementById('forecastPlot').innerHTML = ''; // Clear existing content
+        document.getElementById('forecastPlot').appendChild(imgElement); // Add the image element
+    })    
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+  }
+  
+  // Make sure to have your Flask server ready to handle this JSON data on '/submit_form'
+  // and have 'start-date' and 'end-date' input fields on your HTML page.
+  
+  
+function searchCategories() {
+  var searchInput = document.getElementById("searchInput").value.toLowerCase(); // Convert search input to lowercase for case-insensitive matching
+  var categories = ["Cereals and products", "Clothing", "Clothing and footwear", "Consumer Food Price Index", "Education", "Egg", "Food and beverages", "Footwear", "Fruits", "Fuel and light", "Health", "Household goods and services", "Housing" , "Meat and fish" , "Milk and products", "Miscellaneous", "Non-alcoholic beverages", "Oils and fats", "Pan; tobacco; and intoxicants", "Personal care and effects", "Prepared meals; snacks; sweets etc.", "Pulses and products", "Recreation and amusement", "Spices", "Sugar and confectionery", "Transport and communication", "Vegetables"];
+  var suggestions = categories.filter(category => category.toLowerCase().includes(searchInput)); // Filter categories based on search input
+  displaySuggestions(suggestions);
+}
+
+function displaySuggestions(suggestions) {
+  var suggestionsList = document.getElementById("suggestionsList");
+  suggestionsList.innerHTML = "";
+
+  suggestions.forEach(function(suggestion) {
+      var suggestionItem = document.createElement("div");
+      suggestionItem.textContent = suggestion;
+      suggestionsList.appendChild(suggestionItem);
+  });
+}
+
+// Event listener for input in the search bar
+document.getElementById("searchInput").addEventListener("input", function() {
+  searchCategories();
+});
+
+// function selectInput(list){
+//   inputBox.value = list.innerHTML;
+//   resultBox.innerHTML='';
+// }
